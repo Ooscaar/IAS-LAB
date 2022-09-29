b3cd8b6786ca284c25439f4db347b61a69015b7d
Oscar Perez <ooscar.perez@protonmail.com>
2022-09-29 16:30:52 +0200

    frontend: add json header to fetch requests

--- frontend/api.js
+++ frontend/api.js
@@ -1,29 +1,32 @@
 export class Api {
+  static logIn(username, password) {
+    fetch("/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    }).then((res) => {
+      if (res.status === 200) window.updatePath("/");
+      else alert(`Error ${res.status}`);
+    });
+  }
 
-    static logIn(username, password) {
-        fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({username: username, password: password})
-        })
-        .then(res => {
-            if(res.status === 200) window.updatePath('/');
-            else alert(`Error ${res.status}`);
-        })
+  static register(username, password1, password2) {
+    if (password1 !== password2) {
+      alert("Passwords must match");
+      return;
     }
 
-    static register(username, password1, password2) {
-        if(password1 !== password2) {
-            alert('Passwords must match');
-            return;
-        }
-
-        fetch('/api/users/register', {
-            method: 'POST',
-            body: JSON.stringify({username: username, password: password1})
-        }).then(res => {
-            if(res.status === 200) this.logIn(username, password1);
-            else alert(`Error ${res.status}`);
-        });
-    }
-
+    fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password1 }),
+    }).then((res) => {
+      if (res.status === 200) this.logIn(username, password1);
+      else alert(`Error ${res.status}`);
+    });
+  }
 }
