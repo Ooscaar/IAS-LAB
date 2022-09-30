0775a0e123fc72bc2a0c8d7c296e0668225ac225
Oscar Perez <ooscar.perez@protonmail.com>
2022-09-30 14:53:06 +0200

    backend: introduce new endpoints and add password hashing

--- backend/src/api/messages.ts
+++ backend/src/api/messages.ts
@@ -1,11 +1,76 @@
 import express from "express";
+import { prisma } from "../client";
 
 export const messages = express.Router();
 
-messages.post("/register", async (req, res) => {
-    const { username, password } = req.body as { username: string, password: string };
+/**
+ * GET /api/messages/:postId
+ */
+messages.get("/:postId", async (req, res) => {
+    const { postId } = req.params
 
-    if (!username || !password) {
-        res.status(422).json({ status: "error", message: "Missing username or password" });
+    const postIdAsNumber = Number(postId)
+
+    if (postIdAsNumber === NaN) {
+        return res.status(422).json({ message: "Invalid post id" })
     }
+
+    const post = await prisma.post.findUnique({
+        where: { id: postIdAsNumber },
+        include: {
+            messages: { include: { author: true } }
+        }
+    })
+
+    if (!post) {
+        return res.status(404).json({ message: "Post not found" })
+    }
+
+    res.status(200).json({
+        messages: post.messages.map(message => ({
+            id: message.id,
+            owner: message.author.userName,
+            content: message.content
+        }))
+    })
+})
+
+
+/**
+ * POST /api/messages/:postId
+ */
+messages.post("/:postId", async (req, res) => {
+    const userId = (req as any).userId
+
+    const { postId } = req.params
+
+    const postIdAsNumber = Number(postId)
+
+    if (postIdAsNumber === NaN) {
+        return res.status(422).json({ message: "Invalid post id" })
+    }
+
+    const { message } = req.body as { message: string };
+
+    if (!message) {
+        res.status(422).json({ message: "Missing message" });
+    }
+
+    const post = await prisma.post.findUnique({
+        where: { id: postIdAsNumber },
+    })
+
+    if (!post) {
+        return res.status(404).json({ message: "Post not found" })
+    }
+
+    await prisma.message.create({
+        data: {
+            content: message,
+            authorId: userId,
+            postId: postIdAsNumber
+        }
+    })
+
+    res.status(200).send()
 })
