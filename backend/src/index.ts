c68433ed8944365604c9c988c0c6356a8c58cb84
Oscar Perez <operez@eseleos.com>
2022-11-24 08:13:13 +0100

    backend: fix routes handled by html

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { messages, posts, users } from "./api";
 
 const app = express();
-const port = 8080;
+const port = 8085;
 
 app.use(morgan("combined"))
 app.use(cookieParser())
@@ -15,15 +15,18 @@ app.use(express.json())
 // Static files from frontend folder
 app.use(express.static(path.join(__dirname, "../../frontend")));
 
-app.get("/", (req, res) => {
+// First API endpoints
+app.use("/api/users", users)
+app.use("/api/posts", posts)
+app.use("/api/messages", messages)
+
+// All other requests go to index.html
+app.get("*", (req, res) => {
     // Serve index.html
     res.sendFile(path.join(__dirname, "../../frontend/index.html"));
 });
 
 
-app.use("/api/users", users)
-app.use("/api/posts", posts)
-app.use("/api/messages", messages)
 
 app.listen(port, () => {
     console.log(`[*] Server listening on port ${port}`);
